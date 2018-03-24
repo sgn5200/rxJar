ffced48999bae1722e57f6907008036fdbe1ad01
shangguangneng <sgn5200@sina.com>
2018-03-24 12:07:22 +0800

    test git

--- src/main/java/com/lansent/cannan/app/App.java
+++ src/main/java/com/lansent/cannan/app/App.java
@@ -2,15 +2,6 @@ package com.lansent.cannan.app;
 
 import android.app.Application;
 
-import com.google.gson.reflect.TypeToken;
-import com.lansent.cannan.api.ApiClient;
-import com.lansent.cannan.api.BaseResponse;
-import com.lansent.cannan.api.BaseSub;
-import com.lansent.cannan.api.URLParam;
-import com.lansent.cannan.util.RxEvent;
-
-import io.reactivex.functions.Consumer;
-
 /**
  * Created by Cannan on 2017/9/26 0026.
  * 全局 Application
@@ -32,42 +23,47 @@ public class App extends Application {
 		super.onCreate();
 		mContext = this;
 
-		RxEvent.getInstance().register(BaseResponse.class, new Consumer<BaseResponse>() {
-			@Override
-			public void accept(BaseResponse ba) throws Exception {
-
-			}
-		}, new Consumer<Throwable>() {
-			@Override
-			public void accept(Throwable throwable) throws Exception {
-
-			}
-		});
-
-		RxEvent.getInstance().unRegister(BaseResponse.class);
-
-
-
-		ApiClient.getInstance().request(new URLParam(""), new TypeToken<BaseResponse<String>>() {}, this)
-				.subscribe(new BaseSub<BaseResponse<String>>() {
-					@Override
-					public void callSuccess(BaseResponse<String> response) {
-
-					}
-
-					@Override
-					public void CallFailure(BaseResponse<String> response) {
-
-					}
-
-					@Override
-					public void callError(String e) {
-
-					}
-				});
-
-		BaseResponse baseResponse = new BaseResponse();
-		RxEvent.getInstance().post(baseResponse);
+//		RxEvent.getInstance().register(BaseResponse.class, new Consumer<BaseResponse>() {
+//			@Override
+//			public void accept(BaseResponse ba) throws Exception {
+//
+//			}
+//		}, new Consumer<Throwable>() {
+//			@Override
+//			public void accept(Throwable throwable) throws Exception {
+//
+//			}
+//		});
+//
+//		RxEvent.getInstance().unRegister(BaseResponse.class);
+//
+//
+//
+//		ApiClient.getInstance().request(new URLParam(""), new TypeToken<BaseResponse<String>>() {}, this)
+//				.subscribe(new BaseSub<BaseResponse<String>>() {
+//					@Override
+//					public void callSuccess(BaseResponse<String> response) {
+//
+//					}
+//
+//					@Override
+//					public void CallFailure(BaseResponse<String> response) {
+//
+//					}
+//
+//					@Override
+//					public void callError(String e) {
+//
+//					}
+//				});
+//
+//		BaseResponse baseResponse = new BaseResponse();
+//		RxEvent.getInstance().post(baseResponse);
+//		git init
+//		git add README.md
+//		git commit -m "first commit"
+//		git remote add origin https://github.com/sgn5200/rxJar.git
+//		git push -u origin master
 	}
 
 }
